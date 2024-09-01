7976da16cc63c0a1f3c7d5e76ba2752eb4b92c4f
Gabriele Mercolino <gmercolino2003@gmail.com>
2024-09-01 12:13:04 +0200

    feat: now using GM_openInTab function to open tab and closing tab after 2 seconds

--- open_giveaways.user.js
+++ open_giveaways.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
-// @name         IG Auto Open Giveaway pages 
+// @name         IG Auto Open Giveaway pages
 // @namespace    https://github.com/gabrielemercolino/ParticipateIGGiveaway
-// @version      2024-08-31
+// @version      1.2
 // @description  automatically participate Instant Gaming giveaway
 // @author       gabrielemercolino
 // @match        *://*/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        GM_registerMenuCommand
 // @grant        GM_getResourceText
+// @grant        GM_openInTab
 // @run-at       document-idle
 // @license      MIT
 // @downloadURL  https://github.com/gabrielemercolino/ParticipateIGGiveaway/open_giveaways.user.js
@@ -17,20 +18,17 @@
 (function() {
     'use strict';
 
-    var giveaways = JSON.parse(GM_getResourceText("giveaways"))
-
-    function openInNewTab(url) {
-        window.open(url, '_blank');
-    }
-
     function sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
 
     async function openAll(){
+        var giveaways = JSON.parse(GM_getResourceText("giveaways"))
+
         for (const give of giveaways){
-            openInNewTab("https://www.instant-gaming.com/giveaway/" + give);
+            var window = GM_openInTab("https://www.instant-gaming.com/giveaway/" + give);
             await sleep(2000);
+            window.close();
         }
     }
 
