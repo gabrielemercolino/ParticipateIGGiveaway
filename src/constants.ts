8bbdb4a4cc329267bacff4aea8bb9c4022ee975c
Gabriele Mercolino <gmercolino2003@gmail.com>
2025-05-22 21:46:42 +0200

    chore: added new selector

--- src/constants.ts
+++ src/constants.ts
@@ -3,6 +3,7 @@ export const VERSION = GM.info.script.version;
 export const GIVEAWAYS_REPO = `https://github.com/gabrielemercolino/ParticipateIGGiveaway/releases/download/${VERSION}/giveaways.json`;
 
 export const SELECTORS = {
+  boostSection: ".participation-state.has-participation",
   boostButtons: "a.button.reward.alerts:not(.success)",
   participateButton: "button.button.validate",
   _404: "span.e404",
