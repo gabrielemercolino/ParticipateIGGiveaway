e69b4814fe28600c234c509b80ef339169fb4aab
Gabriele Mercolino <gmercolino2003@gmail.com>
2025-05-22 10:24:31 +0200

    chore: separated metadata

---
+++ tampermonkey.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         IG Auto Open and Participate Giveaway
+// @namespace    https://github.com/gabrielemercolino/ParticipateIGGiveaway
+// @version      __VERSION__
+// @description  automatically participate Instant Gaming giveaway
+// @author       gabrielemercolino
+// @match        https://www.instant-gaming.com/*/
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        GM.registerMenuCommand
+// @grant        GM.xmlHttpRequest
+// @run-at       document-idle
+// @noframes
+// @license      MIT
+// @downloadURL  https://github.com/gabrielemercolino/ParticipateIGGiveaway/releases/latest/download/giveaways.user.js
+// @updateURL    https://github.com/gabrielemercolino/ParticipateIGGiveaway/releases/latest/download/giveaways.user.js
+// ==/UserScript==
